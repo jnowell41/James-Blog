bf4ab2d215e03c8fad7911c07ff73b89b68a72e5
James Nowell <james.nowell@Nology.local>
2019-03-21 15:45:31 +0000

    implemented new functions

--- index.ts
+++ index.ts
@@ -1,4 +1,44 @@
-console.log("blahblahblah");
-console.log("oiiii");
-console.log("this is working");
-window.alert("helllooo");
+class BlogPost {
+    title: string;
+    content:string;
+
+    constructor(title:string, content: string) {
+        this.title = title;
+        this.content = content;
+    }
+}
+
+let blogPosts: BlogPost[] = [];
+
+const createBlogPost = () => {
+    let form = document.getElementById("blog-post");
+    let info = form["blogger-title"].value;
+    let postBox = form["blogger-post"].value;
+
+    blogPosts.push(new BlogPost(info, postBox));
+    logPosts();
+};
+
+function logPosts() {
+
+    var newTitle = document.createElement("h2");
+    var newContent = document.createElement("p");
+    var newDate = document.createElement("p");
+    var dateFunc = new Date();
+    var newDiv = document.createElement("div");
+    var umbrellaElement = document.getElementsByClassName("container")[0];
+        
+        blogPosts.forEach(element => {
+
+        var newContainer = umbrellaElement.appendChild(newDiv);
+
+        newDate.innerHTML = dateFunc.toString();
+        newContainer.insertBefore(newDate, newContainer.childNodes[0]);
+
+        newContent.innerHTML=element.content;
+        newContainer.insertBefore(newContent, newContainer.childNodes[0]);
+
+        newTitle.innerHTML=element.title;
+        newContainer.insertBefore(newTitle, newContainer.childNodes[0]);
+    });
+}
